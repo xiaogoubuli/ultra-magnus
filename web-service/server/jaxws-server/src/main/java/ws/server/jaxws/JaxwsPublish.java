841206e291c9f10889d157a475dc3018b6ac6f2f
xiaogoubuli <xiaogoubuli@163.com>
2023-07-02 22:06:53 +0800

    update address

--- web-service/server/jaxws-server/src/main/java/ws/server/jaxws/JaxwsPublish.java
+++ web-service/server/jaxws-server/src/main/java/ws/server/jaxws/JaxwsPublish.java
@@ -6,14 +6,14 @@ import javax.xml.ws.Endpoint;
 
 /**
  * web service publish
- * http://localhost:8888/ws?wsdl
- * http://localhost:8888/ws?xsd=1
+ * http://localhost:8888/ws/jaxws/hello?wsdl
+ * http://localhost:8888/ws/jaxws/hello?xsd=1
  *
  * @author xiaogoubuli
  */
 public class JaxwsPublish {
     public static void main(String[] args) {
-        String address = "http://localhost:8888/ws";
+        String address = "http://localhost:8888/ws/jaxws/hello";
         Endpoint.publish(address, new HelloServiceImpl());
         System.out.println("web service published.");
     }
